f292db3ca9142a6f0ca51e5a687328f7aedca517
Don Knieriem <dknieriem@newrelic.com>
2023-04-03 15:31:59 -0400

    working w/ drupal theme global

--- src/withColumns.tsx
+++ src/withColumns.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { DecoratorFunction, useEffect } from "@storybook/addons";
-import { useAddonState } from "@storybook/client-api";
+import { useAddonState, useGlobals } from "@storybook/client-api";
 import { GridOverlay } from "./components/ui";
 import { ADDON_ID } from "./constants";
 import { ColumnsProps } from "./types";
+// import { useGlobals } from '@storybook/api';
 
 export const withColumns: DecoratorFunction = (StoryFn, context) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [active] = useAddonState(`${ADDON_ID}_active`);
   const [bgColor] = useAddonState(`${ADDON_ID}_bgColor`);
   const [columns] = useAddonState(`${ADDON_ID}_columns`);
-
+  const [{ drupalTheme }] = useGlobals();
+  console.log(drupalTheme);
   useEffect(() => {
     if (columns) setIsLoaded(true);
   }, [columns]);
