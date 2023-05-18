cdc9da00bfa59c7ec3f4985504ef6415d7609c50
Don Knieriem <dknieriem@newrelic.com>
2023-05-18 11:33:53 -0400

    Works with no params

--- src/fetchStoryHtml.ts
+++ src/fetchStoryHtml.ts
@@ -19,7 +19,34 @@ type StorybookContext = {
 import { ADDON_ID } from "./constants";
 import { useGlobals } from "@storybook/api";
 
-function createNewBody(htmlDoc: Document): HTMLElement {
+function createNewBody(htmlDoc: Document, wrapperDoc: Document): HTMLElement {
+  const wrapperWrapper = wrapperDoc.getElementsByClassName('r01-layout').item(0);
+  const clWrapper = htmlDoc.getElementById('___cl-wrapper');
+  // Extract the missing scripts and re-add them.
+  const scripts = htmlDoc.getElementsByTagName('script');
+  const wrapperScripts = htmlDoc.getElementsByTagName('script');
+  const allScripts = Array.from(scripts).concat(Array.from(wrapperScripts));
+
+  const newBody = wrapperDoc.createElement('body');
+  // Copy the body attributes from the old body to the new, in case there is
+  // anything functionally relevant.
+  wrapperDoc.body.getAttributeNames().forEach((attrName) => {
+    newBody.setAttribute(attrName, wrapperDoc.body.getAttribute(attrName));
+  });
+  const wrapperColumns = Array.from(wrapperWrapper.getElementsByClassName('row').item(0).children); //wrapperDoc.getElementsByClassName('row')
+  wrapperColumns.forEach( (column) => {
+    column.innerHTML = clWrapper.innerHTML;
+  })
+  newBody.innerHTML = wrapperWrapper.innerHTML;
+  // Include the Drupal "js footer" assets, i.e., all the <script> tags in
+  // the <body>.
+  const footerScripts = htmlDoc.createElement('div');
+  footerScripts.append(...allScripts); //footerScripts.append(...Array.from(scripts));
+  newBody.append(footerScripts);
+  return newBody;
+}
+
+function extractClContent(htmlDoc: Document): HTMLElement {
   const clWrapper = htmlDoc.getElementById('___cl-wrapper');
   // Extract the missing scripts and re-add them.
   const scripts = htmlDoc.getElementsByTagName('script');
@@ -40,19 +67,30 @@ function createNewBody(htmlDoc: Document): HTMLElement {
 
 const loadColumnWrapper = async (url: string, context: StorybookContext) => {
   const fetchUrl = new URL(`${url}/_cl_server`);
+  const args = {
+    "background_type":"",
+    "background_color":"",
+    "background_image":"",
+    "margin_bottom":96,
+    "text_color":"",
+    "text_alignment":"left",
+    "column_align":"stretch",
+    "widths":"6-6",
+    "full_bleed":false,
+  };
+  console.log("Load Wrapper context:", context);
   const init: {
     _storyFileName: string;
     _drupalTheme: string;
     _variant?: string;
     _params: string;
   } = {
-    _storyFileName: context.parameters.fileName,
+    _storyFileName: 'web/themes/custom/erno/templates/components/layout/section/section.stories.yml',
     _drupalTheme: context.globals.drupalTheme || context.parameters.drupalTheme,
-    _params: btoa(unescape(encodeURIComponent(JSON.stringify(context.args)))),
+    _params: btoa(unescape(encodeURIComponent(JSON.stringify(args)))),
   };
-
+  // console.log(init);
   fetchUrl.search = new URLSearchParams(init).toString();
-
   // Remove any basic auth embedded into the URL and remove it as it will cause
   // the OPTIONS pre-flight request to fail.
   fetchUrl.username = '';
@@ -60,6 +98,7 @@ const loadColumnWrapper = async (url: string, context: StorybookContext) => {
 
   const response = await fetch(fetchUrl.toString());
   const htmlContents = await response.text();
+  
   if (response.status >= 399) {
     const statusText = `${response.status} (${response.statusText})`;
     let headersText = '';
@@ -78,7 +117,7 @@ const loadColumnWrapper = async (url: string, context: StorybookContext) => {
     const parser = new DOMParser();
     const htmlDoc = parser.parseFromString(htmlContents, 'text/html');
     // Swap the old body for the new.
-    htmlDoc.body = createNewBody(htmlDoc);
+    htmlDoc.body = extractClContent(htmlDoc);
     return htmlDoc.children[0].outerHTML;
   } catch (e) {
     console.error(e);
@@ -96,7 +135,7 @@ const fetchStoryHtml = async (
   url = url.replace(/\/$/, '');
 
   const variant = context.parameters?.options?.variant;
-  console.log(context);
+  // console.log(context);
   // const loadWrapper = context.parameters?.columns?.active;
   // const [globals, updateGlobals] = useGlobals();
   // const active = globals[`${ADDON_ID}_active`];
@@ -133,6 +172,8 @@ const fetchStoryHtml = async (
 
   const response = await fetch(fetchUrl.toString());
   const htmlContents = await response.text();
+  const wrapperContents = await loadColumnWrapper(url, context);
+  // console.log("wrapper contents: ", wrapperContents);
   if (response.status >= 399) {
     const statusText = `${response.status} (${response.statusText})`;
     let headersText = '';
@@ -150,8 +191,10 @@ const fetchStoryHtml = async (
     // We need to extract the HTML for the ___cl-wrapper.
     const parser = new DOMParser();
     const htmlDoc = parser.parseFromString(htmlContents, 'text/html');
+    const wrapperDoc = parser.parseFromString(wrapperContents, 'text/html');
+    // console.log("wrapper doc:", wrapperDoc);
     // Swap the old body for the new.
-    htmlDoc.body = createNewBody(htmlDoc);
+    htmlDoc.body = createNewBody(htmlDoc, wrapperDoc);
     return htmlDoc.children[0].outerHTML;
   } catch (e) {
     console.error(e);
--- src/preset/preview.ts
+++ src/preset/preview.ts
@@ -1,7 +1,9 @@
 import { withColumns } from "../withColumns";
+import fetchStoryHtml from "../fetchStoryHtml";
 
 export const decorators = [withColumns];
 export const parameters = {
+  server: { fetchStoryHtml },
   columns: {
     active: false,
     bgColor: "nr-black",
